3d74b44a86e94185eab125d3401542252c5abea4
richard <richard.porthan@hotmail.com>
2025-08-02 21:28:23 +0300

    Update player movement

--- playerController.gd
+++ playerController.gd
@@ -1,20 +1,20 @@
 extends CharacterBody3D
 
 # How fast the player moves in meters per second.
-@export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
+@export var speed = 8
+@export var jump_speed = 5
+
+var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
+var GRAVITY_DIR = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
 var LOOKAROUND_SPEED = 0.01
-var camera_anglev = 0
-
-var target_velocity = Vector3.ZERO
 
 var rot_x = 0
 var rot_y = 0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _input(event):  		
 	if event.is_action_pressed("ui_cancel"):
@@ -30,30 +30,27 @@ func _input(event):
 
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		# Setting the basis property will affect the rotation of the node.
-		# $Pivot.basis = Basis.looking_at(direction)
-
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	velocity += GRAVITY_DIR * GRAVITY * delta
 	
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	var movement = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		movement += Vector3.RIGHT
+	if Input.is_action_pressed("move_left"):
+		movement += Vector3.LEFT
+	if Input.is_action_pressed("move_back"):
+		movement += Vector3.BACK
+	if Input.is_action_pressed("move_forward"):
+		movement += Vector3.FORWARD
 
-	# Moving the Character
-	velocity = target_velocity
+	movement = movement.normalized() * speed
+	movement = movement.rotated(Vector3.UP, rot_x)
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+	
+	print(movement)
+	velocity.x = movement.x
+	velocity.z = movement.z
+	
 	move_and_slide()
