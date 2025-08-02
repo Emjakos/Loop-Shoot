e6c6d21ac90b6482e77f47ac4a87f10da0d57cde
Emil <emil.kr.backlund@gmail.com>
2025-08-02 18:19:31 +0300

    character controller

---
+++ playerController.gd
@@ -0,0 +1,50 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var mouse_sens = 0.3
+var camera_anglev = 0
+
+var target_velocity = Vector3.ZERO
+
+func _input(event):  		
+	if event is InputEventMouseMotion:
+		#$Camera3D.rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
+		rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
+		var changev=-event.relative.y*mouse_sens
+		if camera_anglev+changev>-50 and camera_anglev+changev<50:
+			camera_anglev+=changev
+			$Camera3D.rotate_x(deg_to_rad(changev))
+
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		# $Pivot.basis = Basis.looking_at(direction)
+
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
