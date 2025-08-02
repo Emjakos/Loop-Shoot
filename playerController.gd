6a4bd9f8283a8b22fed507635d1fd9c949f9d239
CloudHater <mlassheikki@gmail.com>
2025-08-03 01:13:13 +0300

    changed camera behaviour to be more usual (clamped up/down, no longer rotates player hitbox up/down)

--- playerController.gd
+++ playerController.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 # How fast the player moves in meters per second.
 @export var speed = 8
 @export var jump_speed = 5
+@export var max_rot_y = 0.75 * PI
+@export var min_rot_y = -PI/2
 
 var GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
 var GRAVITY_DIR = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
@@ -24,9 +26,16 @@ func _input(event):
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
 		rot_x += -event.relative.x * LOOKAROUND_SPEED
 		rot_y += -event.relative.y * LOOKAROUND_SPEED
+		rot_y = clampf(rot_y, min_rot_y, max_rot_y)
+		
 		transform.basis = Basis()
 		rotate_object_local(Vector3.UP, rot_x)
-		rotate_object_local(Vector3.RIGHT, rot_y)
+		
+		var camera = get_node("Camera3D")
+		camera.transform.basis = Basis()
+		camera.rotate_object_local(Vector3.RIGHT, rot_y)
+		#rotate_object_local(Vector3.UP, rot_x)
+		#rotate_object_local(Vector3.RIGHT, rot_y)
 
 
 func _physics_process(delta):
